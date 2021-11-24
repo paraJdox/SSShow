0dce943a4d1447670155680c4e94ee1accd83066
John Dave Dalmao <johndavedalmao@gmail.com>
2021-11-24 15:08:17 +0800

    Replaced the Privacy Page with the About Page, and deleted the Privacy Page as well

--- SSShow/Controllers/HomeController.cs
+++ SSShow/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace SSShow.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
