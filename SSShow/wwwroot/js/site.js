677ea9ae4b2b8127e2a3833421230fabd0ddab52
John Dave Dalmao <johndavedalmao@gmail.com>
2021-12-09 16:42:28 +0800

    removed the hard-coded <div> tag in _Layout.cshtml, that <div> will now be created in js

--- SSShow/wwwroot/js/site.js
+++ SSShow/wwwroot/js/site.js
@@ -5,10 +5,23 @@
 
 $(document).ready(() => {
     /*==================
-      Modal Operations
+      MODAL OPERATIONS
     ==================*/
     $('button[data-toggle="modal"]').click(function () {
+        // modal area creation (we display the modal in this area)
+        let modalArea = document.createElement("div");
+        modalArea.id = "modal-area";
+
+        // add the modal (ONCE) in the <main> page content so we can "select" it
+        if ($('#modal-area').length <= 0) {
+            let mainPageContent = $('main');
+            mainPageContent.prepend(modalArea);
+        }
+
+        // select the modal area
         let newModal = $('#modal-area');
+
+        /*= DISPLAY MODAL =*/
         let url = $(this).data('url');
         let decodedUrl = decodeURIComponent(url);
 
@@ -18,4 +31,5 @@ $(document).ready(() => {
             newModal.find('.modal').modal('show');
         });
     });
+
 });
