68b855e422e6f884b75ccdf50dacd03a8a2c877a
John Dave Dalmao <johndavedalmao@gmail.com>
2022-03-18 15:20:11 +0800

    SSShow

---
+++ SSShow.Client.Web/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SSShow.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            ViewData["Message"] = "Sign Up";
+            return PartialView("_UserModalPartial");
+        }
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            ViewData["Message"] = "Sign In";
+            return PartialView("_UserModalPartial");
+        }
+    }
+}
---
+++ SSShow.Client.Web/wwwroot/js/site.js
@@ -0,0 +1,35 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(document).ready(() => {
+    /*==================
+      MODAL OPERATIONS
+    ==================*/
+    $('button[data-toggle="modal"]').click(function () {
+        // modal area creation (we display the modal in this area)
+        let modalArea = document.createElement("div");
+        modalArea.id = "modal-area";
+
+        // add the modal (ONCE) in the <main> page content so we can "select" it
+        if ($('#modal-area').length <= 0) {
+            let mainPageContent = $('main');
+            mainPageContent.prepend(modalArea);
+        }
+
+        // select the modal area
+        let newModal = $('#modal-area');
+
+        /*= DISPLAY MODAL =*/
+        let url = $(this).data('url');
+        let decodedUrl = decodeURIComponent(url);
+
+        // ajax get request
+        $.get(decodedUrl).done((data) => {
+            newModal.html(data);
+            newModal.find('.modal').modal('show');
+        });
+    });
+
+});
