3321600000c1e48810c3efb408f6747a367f303a
John Dave Dalmao <johndavedalmao@gmail.com>
2022-03-20 14:55:05 +0800

    created IUserRepository and implemented in in UserRepository class (with just blank methods)

---
+++ src/SSShow.Core/Data/Repositories/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using SSShow.Core.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSShow.Core.Data.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> CreateAsync(User user);
+        Task<IEnumerable<User>> GetAllAsync();
+        Task<User?> GetByEmailAsync(string email);
+        Task<User?> GetByIdAsync(int id);
+        Task<User?> GetByUsernameAsync(string username);
+    }
+}
---
+++ src/SSShow.Infrastructure/Data/Repositories/UserRepository.cs
@@ -0,0 +1,38 @@
+﻿using SSShow.Core.Data.Repositories;
+using SSShow.Core.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSShow.Infrastructure.Data.Repositories
+{
+    internal class UserRepository : IUserRepository
+    {
+        public Task<User> CreateAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> GetByEmailAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> GetByUsernameAsync(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
