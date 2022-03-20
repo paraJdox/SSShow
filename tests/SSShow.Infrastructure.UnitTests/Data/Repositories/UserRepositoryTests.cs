337df7dd27757a4dff10eb1ffbf2c633a84ac95b
John Dave Dalmao <johndavedalmao@gmail.com>
2022-03-20 15:11:05 +0800

    created a UnitTest project for the app's Infrastructure project (with initial setup)

---
+++ tests/SSShow.Infrastructure.UnitTests/Data/Repositories/UserRepositoryTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSShow.Infrastructure.UnitTests.Data.Repositories
+{
+    internal class UserRepositoryTests
+    {
+    }
+}
