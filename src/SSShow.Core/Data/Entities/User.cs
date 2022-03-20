39074b957b0d179078bbb2c4d4e6865341811839
John Dave Dalmao <johndavedalmao@gmail.com>
2022-03-20 14:30:20 +0800

    User entity Created

---
+++ src/SSShow.Core/Data/Entities/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSShow.Core.Data.Entities
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; } = null!;
+
+
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; } = null!;
+
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; } = null!;
+
+
+        [NotMapped]
+        [Required(ErrorMessage = "Password is required")]
+        [Compare(nameof(Password), ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
