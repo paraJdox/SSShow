992fa2f56d0c36c4a453cf1761ffa66e15bcf585
John Dave Dalmao <johndavedalmao@gmail.com>
2022-03-22 14:39:24 +0800

    created and set up AppMainContext

---
+++ src/SSShow.Infrastructure/Data/Contexts/AppMainContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SSShow.Core.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSShow.Infrastructure.Data.Contexts
+{
+    internal class AppMainContext : DbContext
+    {
+        public AppMainContext(DbContextOptions<AppMainContext> options) : base(options)
+        {
+            // This is an empty constructor, but the parameter is needed for dependency injection
+        }
+
+        public DbSet<User> User { get; set; } = null!;
+    }
+}
