e01b896836fae2be42ee9943e3916e3237e7c6f1
John Dave Dalmao <johndavedalmao@gmail.com>
2022-03-22 13:00:30 +0800

    created a test for CreateAsync method (CreateAsync_ShouldReturnUserObjectInTheArgument_IfUserIsNotNull)

--- src/SSShow.Infrastructure/AssemblyInfo.cs
+++ src/SSShow.Infrastructure/AssemblyInfo.cs
@@ -4,4 +4,5 @@
 
 using System.Runtime.CompilerServices;
 
+// To allow this assembly's internal members and types to be accessible for Unit Testing ONLY
 [assembly: InternalsVisibleTo("SSShow.Infrastructure.UnitTests")] 
--- tests/SSShow.Infrastructure.UnitTests/Data/Repositories/UserRepositoryTests.cs
+++ tests/SSShow.Infrastructure.UnitTests/Data/Repositories/UserRepositoryTests.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using SSShow.Core.Data.Entities;
+using SSShow.Infrastructure.Data.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace SSShow.Infrastructure.UnitTests.Data.Repositories
 {
-    internal class UserRepositoryTests
+    public class UserRepositoryTests
     {
+        private readonly UserRepository _sut;
+
+        public UserRepositoryTests()
+        {
+            _sut = new UserRepository();
+        }
+
+        [Fact]
+        public async Task CreateAsync_ShouldReturnUserObjectInTheArgument_IfUserIsNotNull()
+        {
+            // Arrange
+            var newUser = new User
+            {
+                Id = new Random().Next(1, int.MaxValue),
+                Username = "Test UserName",
+                Password = "TestPassword",
+                Email = "test@email.com",
+            };
+
+            // Act
+            var user = await _sut.CreateAsync(newUser);
+
+            // Assert
+            Assert.True(user != null);
+        }
     }
 }
