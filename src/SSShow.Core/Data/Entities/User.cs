d5480ac6cf4de1a62f0a7ea41bd8b1cf3aa497f4
John Dave Dalmao <johndavedalmao@gmail.com>
2022-03-25 15:35:03 +0800

    fixed a typo

--- src/SSShow.Core/Data/Entities/User.cs
+++ src/SSShow.Core/Data/Entities/User.cs
@@ -10,7 +10,7 @@ namespace SSShow.Core.Data.Entities
     public class User
     {
         public int Id { get; set; }
-        public string Username { get; set; } = null!
+        public string Username { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string Password { get; set; } = null!;
         public string ConfirmPassword { get; set; } = null!;
